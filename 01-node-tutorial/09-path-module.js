b78da5f56e55963019149aecceec1afc1de22bc1
TylerDrd <jainilpatel0909@gmail.com>
2024-06-06 00:27:25 +0530

    e

--- 01-node-tutorial/09-path-module.js
+++ 01-node-tutorial/09-path-module.js
@@ -6,7 +6,7 @@ const filePath = path.join('/content/', 'subfolder', 'test.txt')
 console.log(filePath) //joins a sequence of path segments using the platform specific separator and
                       //returns a normalised path
 
-const base = path.basename(filePath)
+const base = path.basename(filePath)//just give the file name out
 console.log(base)
 
 const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt')
